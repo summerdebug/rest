80d45846cd526a0aea4d5b80c7faef4045da3ffa
“summerdebug” <summerdebug@gmail.com>
2019-12-14 22:04:24 +0300

    Add new employee

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -1,6 +1,8 @@
 package payroll;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,9 +16,24 @@ public class EmployeeController {
         this.repository = repository;
     }
 
+    /**
+     * To test it simply open http://localhost:8080/employees in browser
+     */
     @GetMapping("/employees")
     List<Employee> all() {
         return repository.findAll();
     }
 
+    /**
+     * To test it in Soap UI send request:
+     * Method: POST
+     * Endpoint: http://localhost:8080/employees
+     * Header: Content-type, Value: application/json
+     * Body: {"name" : "Bill", "role" : "teacher"}
+     * It will add new employee to the database.
+     */
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee) {
+        return repository.save(newEmployee);
+    }
 }
