27339beed3664866c056d8ff3ccc93cbfdc77976
“summerdebug” <summerdebug@gmail.com>
2019-12-21 14:12:54 +0300

    Added assertions

--- src/test/java/com/example/rest/RestApplicationTests.java
+++ src/test/java/com/example/rest/RestApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.rest;
 
+import com.example.rest.dao.EmployeeRepository;
+import com.example.rest.service.EmployeeController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class RestApplicationTests {
 
+	@Autowired
+	private EmployeeRepository repository;
+
+	@Autowired
+	private EmployeeController controller;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(repository);
+		assertNotNull(controller);
 	}
 
 }
